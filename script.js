17eb61fcd7f50a46b88f57b80fa6d577c77f95b3
Jahan Zaib <zaibu760@gmail.com>
2025-11-04 22:09:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener("click",()=>{
         const value =button.textContent;   //jab btn click ho ga to button k ander wali value variable value k ander store ho gi.
+        if (display.value === "Error") {
+            display.value = "";
+        }
         if(button.classList.contains("allClear")){
             display.value = "";
         }
         else if(button.classList.contains("delete")){
             display.value = display.value.slice(0,-1);
+        }
+          else if (button.classList.contains("percent")) {
+            display.value = parseFloat(display.value) / 100;
         }
 
         else if(button.classList.contains("equal")){
